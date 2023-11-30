3d60defbc3a3490d1ff8ba4d10dfb2eb4ee570b1
Tomáš Chaloupka <chalucha@gmail.com>
2023-11-30 22:45:20 +0100

    added tests with betterC

--- source/mempooled/package.d
+++ source/mempooled/package.d
@@ -8,15 +8,20 @@ version (unittest)
     version (D_BetterC)
     {
         import core.stdc.stdio;
+        import std.meta : AliasSeq;
+
         extern (C) int main()
         {
-            static foreach(u; __traits(getUnitTests, mempooled.fixed))
+            foreach (mod; AliasSeq!(mempooled.dynamic, mempooled.fixed))
             {
-                static if (__traits(getAttributes, u).length)
-                    printf("unittest %s:%d | '" ~ __traits(getAttributes, u)[0] ~ "'\n", __traits(getLocation, u)[0].ptr, __traits(getLocation, u)[1]);
-                else
-                    printf("unittest %s:%d\n", __traits(getLocation, u)[0].ptr, __traits(getLocation, u)[1]);
-                u();
+                static foreach(u; __traits(getUnitTests, mod))
+                {
+                    static if (__traits(getAttributes, u).length)
+                        printf("unittest %s:%d | '" ~ __traits(getAttributes, u)[0] ~ "'\n", __traits(getLocation, u)[0].ptr, __traits(getLocation, u)[1]);
+                    else
+                        printf("unittest %s:%d\n", __traits(getLocation, u)[0].ptr, __traits(getLocation, u)[1]);
+                    u();
+                }
             }
             printf("All unit tests have been run successfully.\n");
             return 0;
